b890fd267aa6c0272419f1363f321feadc734f3d
HenryBenaye <henry.be@outlook.com>
2020-03-23 13:48:29 +0100

    Uitwerking van de exercise

--- index.php
+++ index.php
@@ -6,12 +6,12 @@
 
     $dsn = "mysql:host=$host;dbname=$db;port=3306";
 
-    try {
+try {
         $pdo = new PDO($dsn, $user, $pass);
         echo"connected to db 'Netland'!!";
-    } catch (\PDOException $e) {
+} catch (\PDOException $e) {
         throw new \PDOException($e->getMessage("Not connected"));
-    }
+}
 
 
 ?>
